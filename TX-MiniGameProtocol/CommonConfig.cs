5f9e79b2061d7bbf5b2ffeb09f2605596474188d
congtoulin <congtoulin@tencent.com>
2025-09-01 10:46:43 +0800

    Update CommonConfig.cs

--- TX-MiniGameProtocol/CommonConfig.cs
+++ TX-MiniGameProtocol/CommonConfig.cs
@@ -21,8 +21,8 @@ namespace MiniGameServer {
         // public const string Ip = "127.0.0.1";
         // public const string InnerIp = "127.0.0.1";
         // 内网
-        public const string Ip = "127.0.0.1";
-        public const string InnerIp = "127.0.0.1";
+        public const string Ip = "10.46.47.69";
+        public const string InnerIp = "10.46.47.69";
         public const int Port = 17666;
         public const int ReqPingInterval = 8 * 1000;  // 30s 心跳发送频率
     }
